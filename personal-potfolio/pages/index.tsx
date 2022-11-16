6a390bffbe750c3cf1a663a4be9bdb7d510313bc
Sparsh Bhardwaj <Sparsh.bhardwaj.14.99@gmail.com>
2022-11-16 19:33:51 +0530

    first_commit

---
+++ personal-potfolio/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Personal Portfolio</title>
+      </Head>
+      <Header />
+      {/* hero */}
+      {/* about */}
+      {/* experience */}
+      {/* skills */}
+      {/* projects */}
+      {/* contact Me */}
+    </div>
+  );
+}
